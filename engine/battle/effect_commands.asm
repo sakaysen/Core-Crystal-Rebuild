4c71575a72db55fd62b399633b8d1aa619f7940b
Mr. Jagroo <ravijagroo239@gmail.com>
2024-02-28 09:05:20 -0400

    Removed the gym badges boosts Credit: https://github.com/pret/pokecrystal/wiki/Remove-the-gym-badges-boosts

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1253,12 +1253,6 @@ BattleCommand_Stab:
 	pop de
 	pop hl
 
-	push de
-	push bc
-	farcall DoBadgeTypeBoosts
-	pop bc
-	pop de
-
 	ld a, [wCurType]
 	cp b
 	jr z, .stab
@@ -4734,9 +4728,6 @@ CalcPlayerStats:
 
 	ld a, NUM_BATTLE_STATS
 	call CalcBattleStats
-
-	ld hl, BadgeStatBoosts
-	call CallBattleCore
 
 	call BattleCommand_SwitchTurn
 
