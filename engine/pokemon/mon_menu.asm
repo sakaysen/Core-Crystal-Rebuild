028e95e46767327e37504e35ca5d24ca1d14b8a6
RaJah239 <ravijagroo239@gmail.com>
2025-03-23 06:34:20 -0400

    Perfect accuracy displays as "---" Credit: https://github.com/pret/pokecrystal/wiki/Add-a-Move-Reminder#723-dont-show-a-number-for-accuracy-for-moves-that-never-miss

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -1353,10 +1353,20 @@ PlaceMoveData:
 ; Print move accuracy
 	ld a, [wCurSpecies]
 	ld bc, MOVE_LENGTH
+	ld hl, (Moves + MOVE_EFFECT) - MOVE_LENGTH
+	call AddNTimes
+	ld a, BANK(Moves)
+	call GetFarByte
+
+	cp EFFECT_MIRROR_MOVE
+	jr nc, .perfect_accuracy
+	ld a, [wCurSpecies]
+	ld bc, MOVE_LENGTH
 	ld hl, (Moves + MOVE_ACC) - MOVE_LENGTH
 	call AddNTimes
 	ld a, BANK(Moves)
 	call GetFarByte
+
 	Call ConvertPercentages
 	ld [wBuffer1], a
 	ld de, wBuffer1
@@ -1364,7 +1374,18 @@ PlaceMoveData:
 	hlcoord 5, 12
 	call PrintNum
 
+	jr .print_move_attack
+
+; This prints "---" if the move
+; has perfect accuracy.
+.perfect_accuracy
+	ld de, String_MoveNoPower
+	ld bc, 3
+	hlcoord 5, 12
+	call PlaceString
+
 ; Print move type
+.print_move_attack
 	ld a, [wCurSpecies]
 	ld b, a
 	hlcoord 10, 12
