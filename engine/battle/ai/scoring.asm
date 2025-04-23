fe4afb0c084ac6bc7161aad936712f08a6bc97ba
RaJah239 <ravijagroo239@gmail.com>
2025-04-23 13:57:35 -0400

    Optimised Risky Effects

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -3155,7 +3155,7 @@ AI_Status:
 
 AI_Risky:
 ; Use any move that will KO the target.
-; Risky moves will often be an exception (see below).
+; Selfdestructing moves will often be an exception (see below).
 
 	ld hl, wEnemyAIMoveScores - 1
 	ld de, wEnemyMonMoves
@@ -3179,12 +3179,10 @@ AI_Risky:
 	and a
 	jr z, .nextmove
 
-; Don't use risky moves at max hp.
+; Don't use selfdestructing moves at max hp.
 	ld a, [wEnemyMoveStruct + MOVE_EFFECT]
-	ld de, 1
-	ld hl, RiskyEffects
-	call IsInArray
-	jr nc, .checkko
+	cp EFFECT_SELFDESTRUCT
+	jr nz, .checkko
 
 	call AICheckEnemyMaxHP
 	jr c, .nextmove
@@ -3218,9 +3216,6 @@ endr
 	pop bc
 	pop de
 	jr .checkmove
-
-INCLUDE "data/battle/ai/risky_effects.asm"
-
 
 AI_None:
 	ret
