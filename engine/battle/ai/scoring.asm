a7980c6f1ffc364dd4edf3459326d07b1c669b87
RaJah239 <ravijagroo239@gmail.com>
2025-04-23 14:45:35 -0400

    `AI_Smart_Bide:` optimised

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -937,17 +937,6 @@ AI_Smart_ResetStats:
 	inc [hl]
 	ret
 
-AI_Smart_Bide:
-; 90% chance to discourage this move unless enemy's HP is full.
-
-	call AICheckEnemyMaxHP
-	ret c
-	call Random
-	cp 10 percent
-	ret c
-	inc [hl]
-	ret
-
 AI_Smart_ForceSwitch:
 ; Whirlwind, Roar.
 
@@ -996,6 +985,7 @@ AI_Smart_LeechSeed:
 	inc [hl]
 	ret
 
+AI_Smart_Bide:
 AI_Smart_LightScreen:
 AI_Smart_Reflect:
 ; Over 90% chance to discourage this move unless enemy's HP is full.
