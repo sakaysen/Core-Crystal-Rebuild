fc46753183d3c3ab8ce57158435572764814fcd5
RaJah239 <ravijagroo239@gmail.com>
2025-05-11 13:04:44 -0400

    Roar, Whirlwind, and Struggle are unaffected by accuracy or evasion, like Swift Credit: https://github.com/Rangi42/polishedcrystal

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1572,6 +1572,12 @@ BattleCommand_CheckHit:
 	call GetBattleVar
 	cp EFFECT_ALWAYS_HIT
 	ret z
+	cp EFFECT_FORCE_SWITCH
+	ret z
+	ld a, BATTLE_VARS_MOVE_ANIM
+	call GetBattleVar
+	cp STRUGGLE
+	ret z
 
 	call .StatModifiers
 
