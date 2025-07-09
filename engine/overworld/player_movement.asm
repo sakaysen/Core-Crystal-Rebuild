f55da6cd13cfe832e9b4105c5c7603d2e51bd467
RaJah239 <ravijagroo239@gmail.com>
2025-07-09 15:32:27 -0400

    Fix issue with Ledge tiles play jump noise if you face them Credit: https://github.com/fellowship-of-the-roms/CrystalShireEngine/commit/d1a1efb154c583047f38a9512d02fe1dc3b9f468

--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -114,6 +114,9 @@ endc
 	ld a, [wWalkingDirection]
 	cp STANDING
 	jr z, .Standing
+	ld a, [wPlayerStepType]
+	cp STEP_TYPE_TURN
+	jr z, .Standing
 
 ; Walking into an edge warp won't bump.
 	ld a, [wWalkingIntoEdgeWarp]
@@ -428,6 +431,9 @@ endc
 	add hl, de
 	ld a, [wFacingDirection]
 	and [hl]
+	jr z, .DontJump
+	ld a, [wPlayerStepType]
+	cp STEP_TYPE_TURN
 	jr z, .DontJump
 
 	ld de, SFX_JUMP_OVER_LEDGE
