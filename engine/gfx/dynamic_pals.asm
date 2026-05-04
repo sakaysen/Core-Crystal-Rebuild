fellowship-of-the-roms/CrystalShireEngine

Some Dynamic Pal Improvements

engine/gfx/dynamic_pals.asm
@@ -1,19 +1,32 @@
 ClearSavedObjPals::
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wUsedObjectPals)
+	ldh [rSVBK], a
+
 	xor a
 	ld [wUsedObjectPals], a
 	ld hl, wUsedObjectPals
 	ld bc, wNeededPalIndex - wUsedObjectPals
 	ld a, -1
-	jmp ByteFill
+	rst ByteFill
+
+	pop af
+	ldh [rSVBK], a
+	ret
 
 DisableDynPalUpdates::
+	push hl
 	ld hl, wPalFlags
 	set DISABLE_DYN_PAL_F, [hl]
+	pop hl
 	ret
 
 EnableDynPalUpdates::
+	push hl
 	ld hl, wPalFlags
 	res DISABLE_DYN_PAL_F, [hl]
+	pop hl
 	; fallthrough to manually run CheckForUsedObjPals
 
 CheckForUsedObjPals::
@@ -22,9 +35,14 @@ CheckForUsedObjPals::
 	push bc
 	push af
 
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wUsedObjectPals)
+	ldh [rSVBK], a
+
 	ld hl, wPalFlags
 	bit DISABLE_DYN_PAL_F, [hl]
-	jmp nz, PopAFBCDEHL
+	jr nz, .done
 
 	; reset all wUsedObjectPals bits
 	xor a
@@ -43,6 +61,9 @@ CheckForUsedObjPals::
 	; If this flag was set, it's time to reset it
 	ld hl, wPalFlags
 	res NO_DYN_PAL_APPLY_F, [hl]
+.done
+	pop af
+	ldh [rSVBK], a
 	jmp PopAFBCDEHL
 
 ScanObjectStructPals:
@@ -67,6 +88,7 @@ ScanObjectStructPals:
 	call MarkUsedPal
 	; Then load the return into OBJECT_PALETTE, which corresponds
 	; to OBJ 0 - OBJ 7
+	jr nc, .skip
 	and PALETTE_MASK
 	ld c, a
 	ld hl, OBJECT_PALETTE
@@ -106,6 +128,8 @@ MarkUsedPal:
 	; load any pals yet, just mark the still active pals
 	ld hl, wPalFlags
 	bit SCAN_OBJECTS_FIRST_F, [hl]
+	scf
+	ccf
 	jr nz, .done
 
 	ld b, a
@@ -165,5 +189,6 @@ MarkUsedPal:
 	pop bc
 	ld a, c
 
+	scf
 .done
 	jmp PopBCDEHL

engine/menus/start_menu.asm
@@ -139,10 +139,8 @@ StartMenu::
 	jr .ReturnEnd2
 
 .ReturnRedraw:
-	call .Clear
-	jmp .Reopen
-
-.Clear:
+	farcall ClearSavedObjPals
+	farcall DisableDynPalUpdates
 	call ClearBGPalettes
 	call ExitMenu
 	call ReloadTilesetAndPalettes
@@ -150,7 +148,8 @@ StartMenu::
 	call DrawVariableLengthMenuBox
 	call .DrawBugContestStatus
 	call UpdateSprites
-	jmp FinishExitMenu
+	call FinishExitMenu
+	jmp .Reopen
 
 .MenuHeader:
 	db MENU_BACKUP_TILES ; flags

sakaysen/Core-Crystal-Rebuild

engine/gfx/dynamic_pals.asm
@@ -1,14 +1,25 @@
 ClearSavedObjPals::
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wUsedObjectPals)
+	ldh [rSVBK], a
+
 	xor a
 	ld [wUsedObjectPals], a
 	ld hl, wUsedObjectPals
 	ld bc, wNeededPalIndex - wUsedObjectPals
 	ld a, -1
-	jp ByteFill
+	call ByteFill
+
+	pop af
+	ldh [rSVBK], a
+	ret
 
 DisableDynPalUpdates::
+	push hl
 	ld hl, wPalFlags
 	set DISABLE_DYN_PAL_F, [hl]
+	pop hl
 	ret
 
 EnableDynPalUpdates::
@@ -22,9 +33,14 @@ CheckForUsedObjPals::
 	push bc
 	push af
 
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wUsedObjectPals)
+	ldh [rSVBK], a
+
 	ld hl, wPalFlags
 	bit DISABLE_DYN_PAL_F, [hl]
-	jp nz, PopAFBCDEHL
+	jr nz, .done
 
 	; reset all wUsedObjectPals bits
 	xor a
@@ -43,6 +59,9 @@ CheckForUsedObjPals::
 	; If this flag was set, it's time to reset it
 	ld hl, wPalFlags
 	res NO_DYN_PAL_APPLY_F, [hl]
+.done
+	pop af
+	ldh [rSVBK], a
 	jp PopAFBCDEHL
 
 ScanObjectStructPals:
@@ -67,6 +86,7 @@ ScanObjectStructPals:
 	call MarkUsedPal
 	; Then load the return into OBJECT_PALETTE, which corresponds
 	; to OBJ 0 - OBJ 7
+	jr nc, .skip
 	and PALETTE_MASK
 	ld c, a
 	ld hl, OBJECT_PALETTE
@@ -106,6 +126,8 @@ MarkUsedPal:
 	; load any pals yet, just mark the still active pals
 	ld hl, wPalFlags
 	bit SCAN_OBJECTS_FIRST_F, [hl]
+	scf
+	ccf
 	jr nz, .done
 
 	ld b, a
@@ -165,5 +187,6 @@ MarkUsedPal:
 	pop bc
 	ld a, c
 
+	scf
 .done
 	jp PopBCDEHL

engine/menus/start_menu.asm
@@ -140,10 +140,8 @@ StartMenu::
 	jr .ReturnEnd2
 
 .ReturnRedraw:
-	call .Clear
-	jp .Reopen
-
-.Clear:
+	farcall ClearSavedObjPals
+	farcall DisableDynPalUpdates
 	call ClearBGPalettes
 	call Call_ExitMenu
 	call ReloadTilesetAndPalettes
@@ -151,9 +149,8 @@ StartMenu::
 	call DrawVariableLengthMenuBox
 	call .DrawBugContestStatus
 	call UpdateSprites
-	call GSReloadPalettes
 	call FinishExitMenu
-	ret
+	jp .Reopen
 
 .MenuHeader:
 	db MENU_BACKUP_TILES ; flags